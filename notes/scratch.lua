e9cdf6a25fa54187fd9bd1b74d71e686bc8254fd
Harry Gallagher <harrygallagher4@gmail.com>
2022-01-25 20:48:23 -0600

    clean up some extra files

--- notes/scratch.lua
+++ notes/scratch.lua
@@ -1,14 +1,27 @@
-local function use(t)
-    pr(t)
-end
-local function lab(t)
-    local repo = t[1]
-    local parts = vim.split(repo, '/')
-    if #parts ~= 2 then return use(t) end
-    local user = parts[1]; local name = parts[2]
-    t[1] = string.format('https://gitlab.com/%s/%s.git', user, name)
-    t['as'] = name
-    return use(t)
+local M = {}
+
+local fn = vim.fn
+local pack_path = fn.stdpath("data") .. "/site/pack"
+local fmt = string.format
+
+-- prints install/packadd commands instead of executing them. for testing
+function M.Plugin(s)
+    local repo = table.remove(vim.split(s, '/'))
+    local url = (string.match(s,'^[^/]+/[^/]+$') and fmt('https://github.com/%s',s)) or s
+    local install_path = fmt('%s/packer/start/%s', pack_path, repo)
+    if fn.empty(fn.glob(install_path)) == 1 then
+        print(fmt('cloning plugin %s into %s', s, install_path))
+        print(fmt('!git clone %s %s', url, install_path))
+        print(fmt('packadd %s', repo))
+    else -- this clause should not appear in init.lua version
+        print(fmt('detected plugin: %s\nas: %s\nin: %s\n', s, url, install_path))
+    end
 end
 
-lab { 'yorickpeterse/nvim-pqf', config = function () end }
+M.Plugin('wbthomason/packer.nvim')
+M.Plugin('Olical/aniseed')
+M.Plugin('lewis6991/impatient.nvim')
+M.Plugin('harrygallagher4/fake.nvim')
+
+vim._scratch = M
+
