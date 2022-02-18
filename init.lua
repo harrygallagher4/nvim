f6d20c11095933be3e5904cd261c5d15306dc920
Harry Gallagher <harrygallagher4@gmail.com>
2022-02-17 18:15:12 -0600

    changes to init ordering & bootstrapping

--- init.lua
+++ init.lua
@@ -1,18 +1,17 @@
 -- adapted from @Olical/dotfiles, added support for non-github plugins
-local execute = vim.api.nvim_command
-local fn = vim.fn
-local pack_path = fn.stdpath("data") .. "/site/pack"
+local pack_path = vim.fn.stdpath("data") .. "/site/pack"
 local fmt = string.format
+local function exe(...) vim.cmd(fmt(...)) end
 
 function Plugin(s)
-    local repo = table.remove(vim.split(s, '/'))
-    local url = (string.match(s,'^[^/]+/[^/]+$') and fmt('https://github.com/%s',s)) or s
-    local install_path = fmt('%s/packer/start/%s', pack_path, repo)
-    if fn.empty(fn.glob(install_path)) == 1 then
-        execute(fmt('"echomsg installing plugin %s..."', s))
-        execute(fmt('!git clone %s %s', url, install_path))
-        execute(fmt('packadd %s', repo))
-    end
+  local repo = table.remove(vim.split(s, '/'))
+  local url = (string.match(s,'^[^/]+/[^/]+$') and fmt('https://github.com/%s',s)) or s
+  local install_path = fmt('%s/packer/start/%s', pack_path, repo)
+  if vim.fn.empty(vim.fn.glob(install_path)) == 1 then
+    exe('"echomsg installing plugin %s..."', s)
+    exe('!git clone %s %s', url, install_path)
+    exe('packadd %s', repo)
+  end
 end
 
 -- Bootstrap essential plugins required for installing and loading the rest.
@@ -20,13 +19,7 @@ Plugin('wbthomason/packer.nvim')
 Plugin('Olical/aniseed')
 Plugin('lewis6991/impatient.nvim')
 
--- `init` module is just empty so:
---   1. fennel is compiled
---   2. packer startup sets luarocks path
---   3. impatient runs and caches compiled fennel + luarocks
---   4. dotfiles.init runs
-require'aniseed.env'.init { module = 'init', compile = true }
-require'plugins.startup'
 require'impatient'
-require'dotfiles'
+require'plugins.startup'
+require'aniseed.env'.init { module = 'dotfiles', compile = false }
 
