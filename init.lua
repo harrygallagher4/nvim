26554e931ee969506947abe5acffb63808e83c35
Harry Gallagher <harrygallagher4@gmail.com>
2022-02-19 02:12:00 -0600

    shorten init.lua a little more... i'm really just golfing at this point

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 -- adapted from @Olical/dotfiles, added support for non-github plugins
-local pack_path = vim.fn.stdpath("data") .. "/site/pack"
+local pack_path = vim.fn.stdpath('data') .. '/site/pack'
 local fmt = string.format
 local function exe(...) vim.cmd(fmt(...)) end
 
 function Plugin(s)
   local repo = table.remove(vim.split(s, '/'))
-  local url = (string.match(s,'^[^/]+/[^/]+$') and fmt('https://github.com/%s',s)) or s
+  local url = s:match'^[^/]+/[^/]+$' and fmt('https://github.com/%s',s) or s
   local install_path = fmt('%s/packer/start/%s', pack_path, repo)
   if vim.fn.empty(vim.fn.glob(install_path)) == 1 then
     exe('"echomsg installing plugin %s..."', s)
@@ -15,9 +15,9 @@ function Plugin(s)
 end
 
 -- Bootstrap essential plugins required for installing and loading the rest.
-Plugin('wbthomason/packer.nvim')
-Plugin('Olical/aniseed')
-Plugin('lewis6991/impatient.nvim')
+Plugin'wbthomason/packer.nvim'
+Plugin'Olical/aniseed'
+Plugin'lewis6991/impatient.nvim'
 
 require'impatient'
 require'plugins.startup'
