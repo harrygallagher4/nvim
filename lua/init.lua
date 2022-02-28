ea83107fc56a2ab3123a03496be539ee20b3888f
Harry Gallagher <harrygallagher4@gmail.com>
2022-02-27 22:41:41 -0600

    break some init.lua stuff into functions so it's more readable

--- lua/init.lua
+++ lua/init.lua
@@ -3,14 +3,23 @@ local pack_path = vim.fn.stdpath('data') .. '/site/pack'
 local fmt = string.format
 local function exe(...) vim.cmd(fmt(...)) end
 
+local function github_repo_or_url(s)
+  return s:match'^[^/]+/[^/]+$' and fmt('https://github.com/%s', s) or s
+end
+
+local function has_start_plugin(name)
+  local install_path = fmt('%s/packer/start/%s', pack_path, name)
+  return (vim.fn.empty(vim.fn.glob(install_path)) == 1), install_path
+end
+
 function Plugin(s)
-  local repo = table.remove(vim.split(s, '/'))
-  local url = s:match'^[^/]+/[^/]+$' and fmt('https://github.com/%s',s) or s
-  local install_path = fmt('%s/packer/start/%s', pack_path, repo)
-  if vim.fn.empty(vim.fn.glob(install_path)) == 1 then
+  local plugin_name = table.remove(vim.split(s, '/'))
+  local url = github_repo_or_url(s)
+  local empty, install_path = has_start_plugin(plugin_name)
+  if empty then
     exe('"echomsg installing plugin %s..."', s)
     exe('!git clone %s %s', url, install_path)
-    exe('packadd %s', repo)
+    exe('packadd %s', plugin_name)
   end
 end
 
